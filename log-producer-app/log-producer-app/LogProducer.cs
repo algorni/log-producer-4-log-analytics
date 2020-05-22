1590aa50b4cf42d86aecf02d65782cb105ef2c01
algorni <algorni@microsoft.com>
2020-05-22 17:09:37 +0000

    initial sample code

---
+++ log-producer-app/log-producer-app/LogProducer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log_producer_app
+{
+    public class LogProducer
+    {
+        private readonly ILogger _logger;
+
+        public LogProducer(ILogger<LogProducer> logger)
+        {
+            _logger = logger;
+        }
+
+        public void DoSomeStuffAndLogIt()
+        {
+            while (true)
+            {
+                _logger.LogInformation("Something Done");
+
+                Task.Delay(1000).Wait();
+            }
+        }
+    }
+
+    
+}
---
+++ log-producer-app/log-producer-app/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+
+namespace log_producer_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello logs!!");
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+
+            logger.LogInformation("Application started.");
+
+
+            var myClass = serviceProvider.GetService<LogProducer>();
+
+            myClass.DoSomeStuffAndLogIt();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<LogProducer>();
+
+            //we will configure logging here
+
+            var serilogLogger = new LoggerConfiguration()
+               .WriteTo.RollingFile("Logs/log-{HalfHour}.log",
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}")
+               .CreateLogger();
+
+            services.AddLogging(builder =>
+            {
+                builder.SetMinimumLevel(LogLevel.Information);
+                builder.AddSerilog(logger: serilogLogger, dispose: true);
+            });
+        }
+    }
+}
